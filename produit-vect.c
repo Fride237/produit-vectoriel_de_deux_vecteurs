62528645548784f5eb2b3ff690168ef088aa8240
BOYOMO FRIDOLIN SERGE <sergeboyomo92@gmail.com>
2025-09-24 15:32:25 +0100

    Add files via upload

---
+++ produit-vect.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+    float u[3], v[3], produit[3];
+    printf("entrer les coordonnees du vecteur u:");
+    scanf("%f%f%f",&u[0],U[1],u[2]);
+    printf("entrer les coordonnees du vecteur v:");
+    scanf("%f%f%f",&v[0],&v[1],&v[2]);
+
+    produit[o]=u[2]*v[3] - u[3]*v[2];
+    produit[1]= u[3]*v[1] - u[1]*v[3];
+    produit[3]= u[1]*v[2] - u[2]*v[3];
+
+    printf("le produit vectoriel u*v=(%f,%f,%f)/n",produit[0],produit[1],produit[2]);
+    return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
